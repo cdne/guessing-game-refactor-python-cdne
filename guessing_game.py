778163747e2d22e7871bd05b4c9dcf5148b78cfd
cdne <cduneata@gmail.com>
2019-11-22 17:18:17 +0200

    Created a function called guessing_game that takes two random numbers. This function does the same thing as the previous code but is more structured and has less code

--- guessing_game.py
+++ guessing_game.py
@@ -1,33 +1,22 @@
 import math
 import random
-numbers_list = []
-for index in range(10):
-    numbers_list.append(random.randint(1, 99))
-    guess = int(input("Enter an integer from 1 to 99: "))
-    while numbers_list[index] != guess:
-        if guess < numbers_list[index]:
-            print("guess is low")
-            guess = int(input("Enter an integer from 1 to 99: "))
-        elif guess > numbers_list[index]:
-            print("guess is high")
-            guess = int(input("Enter an integer from 1 to 99: "))
-        else:
-            break
-    print("you guessed it!")
 
-numbers_list_two = []
-for index in range(10):
-    numbers_list_two.append(random.randint(1, 49))
-    guess = int(input("Enter an integer from 1 to 49: "))
-    while numbers_list_two[index] != guess:
-        if guess < numbers_list_two[index]:
-            print("guess is low")
-            guess = int(input("Enter an integer from 1 to 49: "))
-        elif guess > numbers_list_two[index]:
-            print("guess is high")
-            guess = int(input("Enter an integer from 1 to 49: "))
-        else:
-            break
-    print("you guessed it!")
+def guessing_game(random_number_one, random_number_two):
+    numbers_list = []
+    for index in range(10):
+        numbers_list.append(random.randint(random_number_one, random_number_two))
+        guess = int(input(f"Enter an integer from {random_number_one} to {random_number_two}: "))
+        while numbers_list[index] != guess:
+            if guess < numbers_list[index]:
+                print("guess is low")
+                guess = int(input(f"Enter an integer from {random_number_one} to {random_number_two}: "))
+            elif guess > numbers_list[index]:
+                print("guess is high")
+                guess = int(input(f"Enter an integer from {random_number_one} to {random_number_two}: "))
+            else:
+                break
+        print("you guessed it!")
 
+guessing_game(1,99)
+guessing_game(1,49)
 
